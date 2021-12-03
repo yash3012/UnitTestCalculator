d89b29c99bf840f17885f2d4005a0a170bf4750c
yash3012 <yashpatel4@gmail.com>
2021-12-03 00:01:30 -0500

    made changes to readme file and added the pic to pylint and cov report and also provided screenshots to the folders created after running main.py along with log file.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-"""This main.py file tests if the calculator can do calculations properly"""
+"""This main.py file tests if the csv files are read?"""
 import time
-import pandas as pd
 from pathlib import Path
+import pandas as pd
 from calc.calculations.addition import Addition
 from calc.calculations.subtraction import Subtraction
 from calc.calculations.multiplication import Multiplication
 from calc.calculations.division import Division
 
 
-add= pd.read_csv("tests/test_data/addition.csv")[:30]
-sub= pd.read_csv("tests/test_data/subtraction.csv")[:30]
-mul= pd.read_csv("tests/test_data/multiplication.csv")[:30]
-div= pd.read_csv("tests/test_data/division.csv")[:30]
+add= pd.read_csv("tests/test_data/addition.csv")
+sub= pd.read_csv("tests/test_data/subtraction.csv")
+mul= pd.read_csv("tests/test_data/multiplication.csv")
+div= pd.read_csv("tests/test_data/division.csv")
 
 try:
     result_directory = Path("log_files/")
@@ -20,12 +20,13 @@ try:
     result_output_directory = Path("after_test/")
     result_output_directory.mkdir(parents = True, exist_ok = True)
 
-except:
+except: # pylint: disable=bare-except
     print("Directories Already Present")
 name_of_files= ["addition.csv", "subtraction.csv", "multiplication.csv", "division.csv"]
 files_for_calc= [add, sub, mul, div]
 operations=[Addition, Subtraction, Multiplication, Division]
-for i in range(len(name_of_files)):
+
+for i, name_of_files in enumerate(name_of_files):
     name = name_of_files[i]
     print(name + " Process Running")
     t = time.time()
@@ -53,9 +54,13 @@ for i in range(len(name_of_files)):
                                    + "\t" + (name_of_files[i][:-4]) + "\t" + str(result) + "\n")
                 files_for_calc[i].to_csv("after_test/" + name_of_files[i])
 
+
             except ZeroDivisionError:
+
                 file_for_exceptions.write(str(j) + "\t" + str(nums[0]) + "\t" + str(nums[1])
-                                          + "\t" + (name_of_files[i][:-4]) + "\t" + "ZeroDivisionError" + "\n")
+
+                            + "\t" + (name_of_files[i][:-4]) + "\t" + "ZeroDivisionError" + "\n")
+
             continue
 
         calculations = operations[i](nums)
--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@
 [![Build Status](https://app.travis-ci.com/yash3012/UnitTestCalculator.svg?branch=main)](https://app.travis-ci.com/yash3012/UnitTestCalculator)
 
 ### added refactored versions travis badge.
+
+## calc_part5 : 
+This part of calculator reads from csv files, 
+performs operations and outputs the result to log file.
+ I have attached screenshots for the pylint, cov report, 
+after_test and log_files
+
+![](img/calc_part5_ss.png)
+![](img/calc_part5_ss1.png)
+![](img/calc_part5_ss2.png)
+![](img/calc_part5_ss3.png)
+![](img/calc_part5_ss4.png)
